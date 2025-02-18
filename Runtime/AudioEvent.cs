7c84961031828dffccf29e98e91731bf02f4e36f
Danae Dekker <hello@danaedekker.com>
2025-02-19 00:04:53 +0100

    Added play instance, stop, and global instance methods to audio events

--- Runtime/AudioEvent.cs
+++ Runtime/AudioEvent.cs
@@ -7,8 +7,12 @@ namespace Audune.Audio
 {
   // Class that defines an audio event
   [Serializable]
-  public sealed class AudioEvent
+  public sealed class AudioEvent : IEquatable<AudioEvent>
   {
+    // Dictionary of global audio event instances
+    private readonly Dictionary<AudioEvent, FMODEventInstance> _globalInstances = new Dictionary<AudioEvent, FMODEventInstance>();
+
+
     // Audio event emitter settings
     [SerializeField, Tooltip("The audio event to play")]
     private EventReference _event;
@@ -30,18 +34,104 @@ namespace Audune.Audio
     // Play the audio event
     public void Play(StateVector vector = null)
     {
+      // Start a one shot instance for the audio event
+      StartOneShotInstance(vector);
+    }
+
+    // Play the audio event and return the instance
+    public FMODEventInstance PlayInstance(StateVector vector = null)
+    {
+      // Start a instance for the audio event
+      var instance = CreateInstance(vector);
+      instance.Start();
+      return instance;
+    }
+
+    // Stop all instances of the audio event
+    public void StopAll()
+    {
+      // Stop all instances of the audio event
+      _event.StopAllInstances();
+    }
+
+    // Return if the audio event has a global instance
+    public bool TryGetGlobalInstance(out FMODEventInstance globalInstance)
+    {
+      // Return the existing global instance if any
+      return _globalInstances.TryGetValue(this, out globalInstance);
+    }
+
+    // Play the audio event on its global instance
+    public void PlayGlobalInstance()
+    {
+      // Return the existing global instance if any
+      if (!TryGetGlobalInstance(out var globalInstance))
+      {
+        // Create a new global instance and store it
+        globalInstance = CreateInstance();
+        if (globalInstance != null)
+          _globalInstances.Add(this, globalInstance);
+      }
+
+      // Start the global instance for the audio event
+      globalInstance?.Start();
+    }
+
+    // Pause the audio event on its global instance
+    public void PauseGlobalInstance(bool isPaused)
+    {
+      // Start the global instance for the audio event
+      if (TryGetGlobalInstance(out var globalInstance))
+        globalInstance.paused = isPaused;
+    }
+
+    // Stop the audio event on its global instance
+    public void StopGlobalInstance()
+    {
+      // Start the global instance for the audio event
+      if (TryGetGlobalInstance(out var globalInstance))
+      {
+        globalInstance.Stop();
+        globalInstance.Dispose();
+      }
+    }
+
+
+    #region Creating and starting instances
+    // Create an instance for the audio event
+    private FMODEventInstance CreateInstance(StateVector vector = null)
+    {
+      // If the type is none or the event is null, then do nothing
+      if (_event.IsNull)
+        return null;
+
+      // Check the type of the event and create an instance
+      if (_type == AudioEventType.OneShot)
+        return _event.CreateInstance(vector);
+      else if (_type == AudioEventType.OneShotAudioTable && TryPickTableKey(out var key))
+        return _event.CreateAudioTableInstance(key, vector);
+      else
+        throw new InvalidOperationException($"{_type} is not a valid audio event type");
+    }
+
+    // Start a one shot instance for the audio event
+    private void StartOneShotInstance(StateVector vector = null)
+    {
       // If the type is none or the event is null, then do nothing
       if (_event.IsNull)
         return;
 
-      // Check the type of the event
+      // Check the type of the event and start a one shot instance
       if (_type == AudioEventType.OneShot)
         _event.StartOneShotInstance(vector);
       else if (_type == AudioEventType.OneShotAudioTable && TryPickTableKey(out var key))
         _event.StartOneShotAudioTableInstance(key, vector);
+      else
+        throw new InvalidOperationException($"{_type} is not a valid audio event type");
     }
+    #endregion
 
-
+    #region Picking audio table keys
     // Pick a key from the audio table
     private bool TryPickTableKey(out string key)
     {
@@ -70,5 +160,26 @@ namespace Audune.Audio
 
       return !string.IsNullOrEmpty(key);
     }
+    #endregion
+
+    #region Equatable implementation
+    // Return if the audio event equals another object
+    public override bool Equals(object obj)
+    {
+      return obj is AudioEvent other && Equals(other);
+    }
+
+    // Return if the audio event equals another event
+    public bool Equals(AudioEvent other)
+    {
+      return _event.Guid == other._event.Guid;
+    }
+
+    // Return the hash code of the audio event
+    public override int GetHashCode()
+    {
+      return HashCode.Combine(_event.Guid);
+    }
+    #endregion
   }
 }
